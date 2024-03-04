7794bed3a5950499192e488ac5b7f64f2319acea
ecamo19 <erickcamo19@gmail.com>
2024-03-04 22:36:29 +1100

    Updating stats

---
+++ scripts/code_clean_and_calculate_growth_measurements.R
@@ -0,0 +1,144 @@
+# Objective ---------------------------------------------------------------------
+# The objective of this script is to create a data frame with different measuares
+# of growth.
+#
+# Measuments
+#
+# + RGR and AGR with the function agr_rgr_calculators
+# + Using harvested a the beginning as initial mean value
+
+
+# Load RGR and AGR FUNCTION ----------------------------------------------------
+source("./R/functions_agr_rgr_calculators.R")
+
+# Load packages ----------------------------------------------------------------
+library(dplyr)
+library(janitor)
+# For date
+library(lubridate)
+# For pivot longer
+library(tidyr)
+# For map
+library(purrr)
+
+# Load data --------------------------------------------------------------------
+raw_data_heigths <- read.csv("./raw_data/data_heights.csv", header = TRUE) %>%
+    clean_names()
+
+
+# Cleaning raw data ------------------------------------------------------------
+
+# Remove x at the beginning of the colnames
+colnames(raw_data_heigths) <- sub("*x", "", colnames(raw_data_heigths))
+
+## Dealing with dates ----------------------------------------------------------
+data_heights_clean <-
+    raw_data_heigths %>%
+
+        # Transform data into longer form
+        pivot_longer(
+            cols = starts_with("2015"),
+            names_to = "date",
+            #names_prefix = "wk",
+            values_to = "height_cm") %>%
+
+        # Convert character columns to factor
+        mutate(across(where(is.character), as.factor),
+
+               # Transform column to date type
+               date = ymd((.$date)))  %>%
+
+        # Create Nfixer category
+        mutate(nfixer = factor(ifelse(spcode == "ec" |
+                                          spcode == "dr" |
+                                          spcode == "gs","fixer",
+
+                                      # else
+                                      "nonfixer"))) %>%
+
+        # Calculate the time intervals along the whole experiment between each
+        # measurement
+        group_by(id) %>%
+        mutate(number_of_days = -1 *(ymd("2015-08-31") - ymd(date))) %>%
+
+        # Remove Harvestatthebegging from the data set
+        filter(!treatment %in% "Harvestatthebegging") %>%
+
+        # Convert date to integer
+        mutate(number_of_days_int = as.integer(number_of_days)) %>%
+
+        # Order the dataframe
+        dplyr::select(id,spcode,treatment,nfixer, everything(),-family)
+
+
+# Calculate RGR and AGR --------------------------------------------------------
+
+data_rgr_agr <-
+    data_heights_clean %>%
+        group_by(id,spcode,treatment,nfixer) %>%
+
+        # Calculate rgr and agr using sourced funcition
+        mutate(rgr = rgr(height_cm, number_of_days_int),
+               agr = agr(height_cm, number_of_days_int)) %>%
+
+        # Get rgr and agr at the final of the experiment
+        filter(number_of_days == 85)
+
+
+# Calculate RGR as a slope -----------------------------------------------------
+
+# Relative growth rate (RGR) of height was calculated as the slope of the natural
+# log transformed height of each individual as a function of time
+
+
+## Nest data -------------------------------------------------------------------
+
+data_height_by_id <-
+    data_heights_clean %>%
+        group_by(id) %>%
+        dplyr::select(id,height_cm,number_of_days_int) %>%
+        nest()
+
+
+## Create model ----------------------------------------------------------------
+
+# This model only returns the slope of each id plant
+
+rgr_model_coef <- function(data) {
+
+    # Constant added for preventing log(0)
+    coef(lm(log(height_cm) ~ log(number_of_days_int + 1), data = data))[[2]]
+}
+
+## map -------------------------------------------------------------------------
+
+rgr_slope <-
+    data_height_by_id %>%
+
+        # map the model to get the slope of each plant (rgr)
+        mutate(rgr_slope = map(data, rgr_model_coef)) %>%
+
+        # Get slope
+        unnest(rgr_slope) %>%
+
+        # Remove col data
+        dplyr::select(-data)
+
+# Join data sets and create data for models ------------------------------------
+
+data_rgr_agr_cleaned <-
+
+    data_rgr_agr %>%
+
+    # Join data by plant's id
+    inner_join(., rgr_slope, by = "id") %>%
+
+    # Remove unused colunms
+    dplyr::select(-c(date,height_cm, number_of_days,number_of_days_int))
+
+# Remove all files except clean data set ---------------------------------------
+
+items <- c("agr", "data_height_by_id", "data_heights_clean", "data_rgr_agr",
+           "raw_data_heigths", "rgr", "rgr_model_coef", "rgr_slope")
+
+remove(items, list = items)
---
+++ scripts/code_join_data_full_dataset.R
@@ -0,0 +1,249 @@
+# Objective -------------------------------------------------------------------
+
+# This script join all trait and biomass data for posterior analysis
+
+# Packages --------------------------------------------------------------------
+library(dplyr)
+library(janitor)
+library(tidyr)
+library(tibble)
+
+# Load data -------------------------------------------------------------------
+
+## RGR and AGR data cleaned ---------------------------------------------------
+source("./scripts/code_clean_and_calculate_growth_measurements.R")
+
+
+## Biomass data ---------------------------------------------------------------
+raw_data_biomass <-
+    read.csv("./raw_data/6_plant_dry_weights_data.csv", header = TRUE) %>%
+	clean_names()
+
+## Ecophys data ---------------------------------------------------------------
+raw_data_ecophys <-
+    read.csv("./raw_data/3_physiology_data.csv", header = TRUE) %>%
+    clean_names() %>%
+    dplyr::select(- c(family))
+
+## Leaf trait data ------------------------------------------------------------
+raw_data_traits <-
+    read.csv("./raw_data/2_leaf_trait_data.csv",header = TRUE) %>%
+    clean_names()
+
+## Isotopes data --------------------------------------------------------------
+raw_data_isotopes <-
+    read.csv("./raw_data/4_isotopes_data.csv", header = TRUE) %>%
+    clean_names()
+
+## Initial height data --------------------------------------------------------
+raw_data_initheight <-
+    read.csv("./raw_data/data_heights.csv", header = TRUE) %>%
+    clean_names()
+
+## RGR and AGR ----------------------------------------------------------------
+# No raw data file loaded here because this data set was cleaned in the file
+# called code_clean_and_calculate_growth_measurements.R
+
+
+# Cleaned data sets -----------------------------------------------------------
+
+## Biomass data----------------------------------------------------------------
+data_biomass_cleaned <-
+	raw_data_biomass %>%
+
+        # Delete Harvested at the beginning treatment
+        #filter(!treatment %in% 'Harvestatthebegging') %>%
+
+        # Calculate biomass related variables
+        mutate(total_biomass = root_dry_weight + stem_dry_weight +
+                               whole_leaf_dry_weight,
+
+
+            # Mass fractions
+            # # % Root Mass Fraction
+            rmf = (root_dry_weight / total_biomass) * 100,
+
+            # % Stem Mass Fractions
+            smf = (stem_dry_weight / total_biomass) * 100,
+
+            # % Leaf Mass Fraction
+            lmf = (whole_leaf_dry_weight / total_biomass) * 100,
+
+            # Root to shoot ratio
+            root_shoot_ratio = root_dry_weight/(stem_dry_weight + whole_leaf_dry_weight)) %>%
+
+        # Order Columns
+        dplyr::select(id, spcode, treatment, rmf, smf, lmf,
+                      whole_leaf_dry_weight, everything()) %>%
+
+        # Convert character columns to factor
+        mutate(across(where(is.character), as.factor))
+
+
+## Ecophys data ---------------------------------------------------------------
+data_ecophys_cleaned <-
+
+    raw_data_ecophys %>%
+
+            # Recode treatment levels
+            mutate(treatment = dplyr::recode(treatment,
+                                      `ambientrain` = "ambientrain",
+                                      `ambientrain+nutrients`= "ambientrain_nutrients",
+                                      `ambientrain+water` = "ambientrain_water",
+                                      `ambientrain+water+nutrients` = "ambientrain_water_nutrients")) %>%
+
+            # Convert character columns to factor
+            mutate(across(where(is.character), as.factor))
+
+
+## Leaf traits data -----------------------------------------------------------
+data_leaf_traits_cleaned <-
+
+	raw_data_traits %>%
+
+    	# Delete Harvested at the beginning treatment
+    	#filter(!treatment %in% 'Harvestatthebeginning') %>%
+    	dplyr::select(-c()) %>%
+
+        # Convert traits to the right units
+        # Transform sla and la to cm2
+        mutate(sla_cm2_g = sla*10000) %>%
+        mutate(la_m2 = la) %>%
+
+        # Remove traits not used in the analysis
+        dplyr::select(-c(family, leaf_fresh_weight, leaf_density, lt, lma, sla,
+                         ldmc, la))  %>%
+
+        # Convert character columns to factor
+        mutate(across(where(is.character), as.factor))
+
+## Isotopes data --------------------------------------------------------------
+
+data_isotopes_cleaned <-
+    raw_data_isotopes %>%
+
+	# Delete Harvested at the beginning treatment
+	#filter(!treatment %in% 'Harvestatthebeginning') %>%
+    dplyr::select(-family) %>%
+
+    # Convert character columns to factor
+    mutate(across(where(is.character), as.factor))
+
+## Initial height data --------------------------------------------------------
+
+data_initheight_cleaned <-
+	raw_data_initheight	%>%
+
+        # Select columns
+        dplyr::select(1:5) %>%
+
+        # Rename column
+        rename(init_height = x20150831) %>%
+
+        #Delete Harvested at the beginning treatment
+        #filter(!treatment %in% 'Harvestatthebegging') %>%
+        dplyr::select(-family) %>%
+
+        # Delete Harvested at the beginning treatment
+        filter(!treatment %in% 'Harvestatthebegging') %>%
+
+        # Convert character columns to factor
+        mutate(across(where(is.character), as.factor))
+
+# Join data sets --------------------------------------------------------------
+
+data_complete <-
+
+	# Join biomass and leaf traits
+    data_biomass_cleaned %>%
+
+        # Add ecophys data
+        inner_join(data_ecophys_cleaned, by = c("id", "spcode", "treatment")) %>%
+
+        # Add leaf traits
+	    inner_join(data_leaftraits_cleaned, by = c("id", "spcode", "treatment")) %>%
+
+        #Add isotope data
+        inner_join(data_isotopes_cleaned, by = c("id", "spcode", "treatment")) %>%
+
+        # Add plant initial height
+        inner_join(data_initheight_cleaned, by = c("id", "spcode", "treatment")) %>%
+
+        # Add plant RGR and AGR
+        inner_join(data_rgr_agr_cleaned, by = c("id", "spcode", "treatment")) %>%
+
+        clean_names()
+
+# Complete dataset ------------------------------------------------------------
+
+data_complete <-
+    data_complete %>%
+
+        # Create Nfixer category
+        mutate(nfixer = factor(ifelse(spcode == "ec" |
+                               spcode == "dr" |
+                               spcode == "gs" ,"fixer", "nonfixer")),
+
+               # Transform Nitrogen percentage to grams
+                N_g = (leaf_dry_weight*perc_n)/100,
+
+                # Transform Nitrogen grams to mol
+                #  1 mol == 14.0067 grams
+                N_mol = N_g/14.0067,
+
+                # Transform Nitrogen percentage to mg
+                N_mg = ((leaf_dry_weight*perc_n)/100)*1000,
+
+                # Calculate Nmass, Narea_g_m2 and Narea_mol_m2
+                Narea_g_m2 = N_g/la_m2,
+
+                Narea_mol_m2 = N_mol/la_m2,
+
+                Nmass_mg_g = N_mg/leaf_dry_weight,
+
+                # Rename the factor levels
+               treatment = factor(recode(data_complete$treatment,ambientrain  = "no_additions",
+                                        ambientrain_water_nutrients = "plus_water_nutrients",
+                                        ambientrain_nutrients = "plus_nutrients",
+                                        ambientrain_water      = "plus_water"))) %>%
+
+        # Order data set columns
+        dplyr::select(id, spcode, nfixer, treatment, init_height, everything())
+
+
+# Data set use for fitting the models -----------------------------------------
+
+#data_for_models <-
+    data_complete %>%
+
+    # Calculate nitrogen use efficiency column
+    # I followed Leaf traits explaining the growth of tree
+    # species planted in a Central Amazonian disturbed area
+    mutate(pnue = amax/ Narea_mol_m2)  %>%
+
+    # select variables that are going to be used in the models
+    dplyr::select(id, spcode, treatment, nfixer, init_height,
+
+                  # Plant preformance
+                  total_biomass,agr, rgr, rgr_slope, root_shoot_ratio,
+
+                  # Mass fractions
+                  rmf, smf, lmf,
+
+                  # Traits
+                  amax, gs, wue, d13c, d15n, pnue, Narea_g_m2) %>%
+
+    # add id to rownames for keep track of the rows
+    column_to_rownames("id") %>%
+    mutate(nfixer = factor(nfixer, levels = c("nonfixer", "fixer")))
+
+
+# Remove all unused data ------------------------------------------------------
+items <- c("data_biomass_cleaned", "data_ecophys_cleaned",
+            "data_initheight_cleaned", "data_isotopes_cleaned",
+            "data_leaftraits_cleaned", "data_rgr_agr_cleaned",
+            "raw_data_biomass", "raw_data_ecophys", "raw_data_initheight",
+           "raw_data_isotopes", "raw_data_traits")
+
+remove(items, list = items)
+ls()
